666c9c39c958beb8b6410b05bec3de22e0e4ffcf
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-02-01 16:13:38 +0100

    Removed NotImplementedCommand

--- adkrover/src/main/java/me/palazzetti/adkrover/arduino/Arduino.java
+++ adkrover/src/main/java/me/palazzetti/adkrover/arduino/Arduino.java
@@ -8,7 +8,7 @@ import me.palazzetti.adkrover.adktoolkit.AdkManager;
 
 public class Arduino {
 
-    public static void executeCommand(AdkManager adkManager, int command, int speed) throws NotImplementedCommand {
+    public static void executeCommand(AdkManager adkManager, int command, int speed) {
         // Create a serial command with format DIRECTION-SPEED (D-SSS)
         String serialCommand = String.valueOf(command) + "-" + String.valueOf(speed);
         adkManager.sendText(serialCommand);
