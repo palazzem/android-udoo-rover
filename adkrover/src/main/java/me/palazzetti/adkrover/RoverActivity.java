dcb4473064da4a83f63b357751c233eb84e2e172
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-02-01 15:22:22 +0100

    Added ADK-toolkit source code

--- adkrover/src/main/java/me/palazzetti/adkrover/RoverActivity.java
+++ adkrover/src/main/java/me/palazzetti/adkrover/RoverActivity.java
@@ -1,13 +1,40 @@
 package me.palazzetti.adkrover;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 
+import me.palazzetti.adkrover.adktoolkit.AdkManager;
+
 public class RoverActivity extends Activity {
+    private AdkManager mAdkManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rover);
+
+        // Start ADK Manager
+        mAdkManager = new AdkManager((UsbManager) getSystemService(Context.USB_SERVICE));
+        registerReceiver(mAdkManager.getUsbReceiver(), mAdkManager.getDetachedFilter());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdkManager.closeAdk();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdkManager.resumeAdk();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mAdkManager.getUsbReceiver());
     }
 }
