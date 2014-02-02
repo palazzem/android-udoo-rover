c73abc9a6ef59174acbca610f2c55a508ab48913
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-02-02 12:58:30 +0100

    Not too generic UrlConnector abstraction

--- adkrover/src/main/java/me/palazzetti/adkrover/utils/Helpers.java
+++ adkrover/src/main/java/me/palazzetti/adkrover/utils/Helpers.java
@@ -1,5 +1,7 @@
 package me.palazzetti.adkrover.utils;
 
+import android.util.Base64;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,5 +21,11 @@ public class Helpers {
 
     public static JSONObject createMockTweet(String text) throws JSONException{
         return new JSONObject().put("id", "240859602684612608").put("text", text);
+    }
+
+    public static String oAuth2TwitterEncoding(String consumerKey, String consumerSecret) {
+        String combinedKey = consumerKey + ":" + consumerSecret;
+
+        return Base64.encodeToString(combinedKey.getBytes(), Base64.NO_WRAP);
     }
 }
---
+++ adkrover/src/main/java/me/palazzetti/adkrover/utils/UrlConnector.java
@@ -0,0 +1,67 @@
+package me.palazzetti.adkrover.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Fast GET/POST connector built on top of HttpURLConnection.
+ * It is *NOT* suitable for generic purpose but it's customized for this project.
+ */
+
+public class UrlConnector {
+    private HttpURLConnection mConnector;
+
+    public UrlConnector(String encodedUrl) throws MalformedURLException, IOException {
+        URL url = new URL(encodedUrl);
+        mConnector = (HttpURLConnection) url.openConnection();
+        mConnector.setReadTimeout(10000);
+        mConnector.setConnectTimeout(15000);
+        mConnector.setUseCaches(false);
+    }
+
+    public void addHeader(String header, String content) {
+        mConnector.setRequestProperty(header, content);
+    }
+
+    public int get() throws ProtocolException, IOException {
+        mConnector.setRequestMethod("GET");
+        mConnector.setDoInput(true);
+
+        return mConnector.getResponseCode();
+    }
+
+    public int post(String parameters) throws ProtocolException, IOException {
+        mConnector.setRequestMethod("POST");
+        mConnector.setDoOutput(true);
+        mConnector.setFixedLengthStreamingMode(parameters.getBytes().length);
+
+        // Make the post
+        PrintWriter output = new PrintWriter(mConnector.getOutputStream());
+        output.print(parameters);
+        output.close();
+
+        return mConnector.getResponseCode();
+    }
+
+    public String getResponse() throws IOException {
+        BufferedReader readerBuffer = new BufferedReader(new InputStreamReader(mConnector.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+
+        while ((line = readerBuffer.readLine()) != null) {
+            response.append(line);
+        }
+
+        return response.toString();
+    }
+
+    public void disconnect() {
+        mConnector.disconnect();
+    }
+}
